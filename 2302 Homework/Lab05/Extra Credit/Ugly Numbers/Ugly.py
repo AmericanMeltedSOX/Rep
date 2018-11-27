88390b6b6920456d6771b899c119ab8d298c78d3
AmericanMeltedSOX <42791805+AmericanMeltedSOX@users.noreply.github.com>
2018-11-27 16:30:41 -0700

    Update Ugly.py

--- 2302 Homework/Lab05/Extra Credit/Ugly Numbers/Ugly.py
+++ 2302 Homework/Lab05/Extra Credit/Ugly Numbers/Ugly.py
@@ -14,17 +14,20 @@ class Ugly:
         
         i = 0
         j = 1
+        # Calculates ugly number to nth position
         while i < n:
             k = j
+            # Tests using a set of 3 prime numbers
             while k % 2 == 0:
                 k = k//2
             while k % 3 == 0:
                 k = k//3
             while k % 5 == 0:
                 k = k//5
-                
+            # Checks if it is ugly number    
             if k == 1:
                 i += 1
             j += 1
-            
+        
+        # Returns ugly number at nth position
         return j - 1
